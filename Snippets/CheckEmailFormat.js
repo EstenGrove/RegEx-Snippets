8250c895cc351916a0660e4f307dd10498955f13
Esten <estengrove99@gmail.com>
2019-02-05 10:19:09 -0700

    additional test case

--- Snippets/CheckEmailFormat.js
+++ Snippets/CheckEmailFormat.js
@@ -12,7 +12,7 @@ const emailReg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 const altEmailReg = /\w+@+\w{1,}.+/i;
 const listedReg = /[a-z0-9\#|$|!|@|%|^|&|*|)|(|)]+@+[a-z]+.+(com|net|us|org|io|co.uk|fm|cn|fr|ru|it|de|com.mx)/;
 const basicEmailReg = /\S+(@)+\S+.(?=\S*)/;
-// "listedReg" literally goes thru and lists all the possibilities for domain extensions and characters to check if the string is valid email format. Only useful if checking for a very specific domain like to block all ".ru" emails or something.
+
 
 // Test cases
 const tc1 = 'georgioGrammand99@gmail.com';
